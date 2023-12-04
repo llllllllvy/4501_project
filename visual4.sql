995b792aefbd01367e445765727f287381107d49
Yueyue Lyu <yl5462@columbia.edu>
2023-12-03 19:20:11 -0500

    update part4 with plot

--- visual4.sql
+++ visual4.sql
@@ -1,5 +1,6 @@
 WITH rent_class AS(
         SELECT t1.zipcode,
+               t1.m2023_09 as rent,
                CASE WHEN t1.m2023_09 BETWEEN 0 AND 1000 THEN '$0-1000'
                     WHEN t1.m2023_09 BETWEEN 1001 AND 2000 THEN '$1001-2000'
                     WHEN t1.m2023_09 BETWEEN 2001 AND 3000 THEN '$2001-3000'
@@ -10,12 +11,14 @@ WITH rent_class AS(
                     WHEN t1.m2023_09 BETWEEN 7001 AND 8000 THEN '$7001-8000'
                 END AS rent_class
         FROM rents AS t1
-        GROUP BY 1,2)
+        GROUP BY 1,2,3)
         SELECT t1.zipcode,
+               t1.rent,
                t1.rent_class,
                COUNT(t2.complaint_id) AS complaint_num
         FROM rent_class AS t1
         LEFT JOIN complaints311 AS t2 ON t1.zipcode = t2.zipcode
         WHERE t2.created_date BETWEEN '2022-10-01T00:00:00.000' AND '2023-09-30T23:59:59.999'
-        GROUP BY 1,2
-        ORDER BY 2
+        GROUP BY 1,2,3
+        ORDER BY 3
+        
