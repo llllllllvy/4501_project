79ac0907905688340ca24084f382d21963003987
Yueyue Lyu <yl5462@columbia.edu>
2023-12-09 17:47:11 -0500

    renew part3-query5

--- query/query5.sql
+++ query/query5.sql
@@ -2,6 +2,7 @@ SELECT t2.zipcode,
        COUNT(t1.tree_id) AS tree_num
 FROM trees AS t1
 JOIN nyczipcodes AS t2 ON ST_Within(ST_Transform(ST_SetSRID(ST_MakePoint(t1.longitude, t1.latitude), 4326), 4326), ST_SetSRID(t2.geom, 4326))
+WHERE  t1.zipcode=t2.zipcode
 GROUP BY t2.zipcode
 ORDER BY 2 DESC
 LIMIT 10
